8a5106539c6724d0b710798f5fdd5c628ef03382
sidhant0802 <sidhantnirupam@gmail.com>
2025-12-20 23:39:22 +0530

    Update theme.js

--- public/js/theme.js
+++ public/js/theme.js
@@ -3,18 +3,15 @@ const toggleBtn = document.getElementById("themeToggle");
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme");
 
-  // icon switch
   toggleBtn.textContent =
     document.body.classList.contains("dark-theme") ? "☀️" : "🌙";
 
-  // save preference
   localStorage.setItem(
     "theme",
     document.body.classList.contains("dark-theme") ? "dark" : "light"
   );
 });
 
-// load saved theme
 window.addEventListener("DOMContentLoaded", () => {
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme === "dark") {
